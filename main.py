501a3da79a4ed92e454429343b3a3c4278eabd24
Max Anderson <m.d.anderson2@student.vu.nl>
2024-12-16 19:49:39 +0100

    Less hardcoded experiment

--- main.py
+++ main.py
@@ -14,39 +14,24 @@ def run_simulation_in_process(num_nodes, num_connections, output_dir, num_steps,
     sim.run(num_steps=num_steps, display_interval=display_interval, metrics_interval=metrics_interval, show=False)
     return f"Simulation completed for {num_connections} connections with seed {random_seed}"
 
-if __name__ == "__main__":
-    profiler = None #cProfile.Profile()
-    if profiler: profiler.enable()
+def run_experiment(experiment_folder, seed_range, connections_range, num_steps, display_interval, metrics_interval, num_nodes):
     start = time.time()
-
-    ## Run in GUI
-    # app = NetworkControlApp() 
-
-
-    ## Quick Run
-    # Simulation(num_nodes=200, num_connections=1990, output_dir="foo", random_seed=42).run(num_steps=50_000, display_interval=1_000, metrics_interval=1_000, show=False)
-
-
-    ## Experiment Run
-    import matplotlib
-    matplotlib.use('Agg')
-
-    experiment_folder = "density3"
     with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
         futures = []
-        for seed in range(1):
-            folder_name = os.path.join(experiment_folder, f"seed_{seed}")
-            for num_connections in range(1000, 19901, 250):
-                scenario_dir = os.path.join(folder_name, f"edges_{num_connections}")
+        for num_connections in connections_range:
+            for seed in seed_range:
+                scenario_dir = os.path.join(experiment_folder, f"seed_{seed}", f"edges_{num_connections}")
+                if os.path.exists(scenario_dir):
+                    continue
                 futures.append(
                     executor.submit(
                         run_simulation_in_process,
-                        num_nodes=200,
+                        num_nodes=num_nodes,
                         num_connections=num_connections,
                         output_dir=scenario_dir,
-                        num_steps=2_000_000,
-                        display_interval=100_000,
-                        metrics_interval=1_000,
+                        num_steps=num_steps,
+                        display_interval=display_interval,
+                        metrics_interval=metrics_interval,
                         random_seed=seed
                     )
                 )
@@ -55,11 +40,57 @@ if __name__ == "__main__":
         for future in futures:
             try:
                 result = future.result()
-                print(result, "at time", {time.time() - start})
+                print(result, "at time", time.time() - start)
             except Exception as e:
                 print(f"Simulation failed: {e}")
 
-    Output.aggregate_metrics(os.path.join("output", experiment_folder), starting_step=4_000)
+    total_time = time.time() - start
+    print(f"End time: {total_time} s = {total_time / 3600} h")
+
+if __name__ == "__main__":
+    profiler = None #cProfile.Profile()
+    if profiler: profiler.enable()
+
+    ## Run in GUI
+    # app = NetworkControlApp() 
+
+
+    ## Quick Run
+    # Simulation(num_nodes=200, num_connections=1990, output_dir="foo", random_seed=42).run(num_steps=50_000, display_interval=1_000, metrics_interval=1_000, show=False)
+    # folder_name = f"finegrained"
+    # run_experiment(folder_name,
+    #                seed_range=range(1,10),
+    #                connections_range=range(1660, 1675),
+    #                num_steps=200_000,
+    #                display_interval=25_000,
+    #                metrics_interval=1_000)
+    # Output.aggregate_metrics(os.path.join("output", folder_name), starting_step=100_000)
+    # for num_connections in range(1660, 1675):
+    #     scenario_dir = os.path.join(folder_name, f"edges_{num_connections}")
+    #     sim = Simulation(num_nodes=200, num_connections=num_connections, output_dir=scenario_dir, random_seed=7)
+    #     # sim.run(num_steps=1_000_000, display_interval=200_000, metrics_interval=10_000, show=False)
+    #     sim.output.post_run_output()
+
+    experiment_folder = "density4"
+    run_experiment(experiment_folder,
+                   seed_range=range(5,6),
+                   connections_range=range(1000, 10001, 1000),
+                   num_steps=200_000,
+                   display_interval=25_000,
+                   metrics_interval=1_000,
+                   num_nodes=200)
+    Output.aggregate_metrics(os.path.join("output", experiment_folder), starting_step=150_000)
+
+    ## Experiment Run
+    # experiment_folder = "density4"
+    # run_experiment(experiment_folder,
+    #                seed_range=range(10),
+    #                connections_range=range(1000, 10001, 100),
+    #                num_steps=200_000,
+    #                display_interval=10_000,
+    #                metrics_interval=1_000,
+    #                num_nodes=200)
+    # Output.aggregate_metrics(os.path.join("output", experiment_folder), starting_step=150_000)
 
     # folder_name = f"density_data_2_seed_7"
     # for num_connections in range(4300, 4601, 300):  # [1200, 1700, 2400, 3000, 3500, 3950, 4150, 4450, 4600, 4850]:  # Adjust connection density
@@ -79,7 +110,7 @@ if __name__ == "__main__":
     #         sim.run(num_steps=20_000_000, display_interval=100_000, metrics_interval=1_000, show=False)
     # Output.aggregate_metrics(os.path.join("output", folder_name))
 
-    print(f"End time: {time.time() - start}")
+
     if profiler: profiler.disable()
 
     # Print profiler stats to sort by cumulative time
