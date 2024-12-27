70e3845296b70201e82a8debae47bc6b8726c5f5
Max Anderson <m.d.anderson2@student.vu.nl>
2024-12-27 11:52:57 +0100

    Run params

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ if __name__ == "__main__":
 
     ## Quick Run
     # Experiment("output").run_simulation(num_nodes=200, num_connections=2_000, output_dir="foo",  num_steps=5_000, display_interval=1_000, metrics_interval=1_000, random_seed=42)
+    PostRunAnalyzer(os.path.join(base_dir, "density3")).aggregate_metrics(os.path.join(base_dir, "density3"), starting_step=4_000_000)
 
     ## Experiment Run
-    experiment_folder = os.path.join(base_dir, "cluster_tracked")
+    experiment_folder = os.path.join(base_dir, "cluster_tracked_percentage")
     experiment = Experiment(experiment_folder)
     experiment.run_experiment(
-                            seed_range=range(1),
-                            nodes_range=[200, 300],
-                            connections_range=[x / 1000.0 for x in range(100, 301, 25)],
+                            seed_range=range(3),
+                            nodes_range=[200, 300, 400],
+                            connections_range=[x / 100.0 for x in range(8, 33, 2)],
                             connections_as_density = True,
                             num_steps=2_000_000,
                             display_interval=250_000,
