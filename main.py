a521261c059b034947059c56c7d45f6605444da8
BaronBrixius <kiqjaq@gmail.com>
2025-01-02 18:52:20 +0100

    Experiment?

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
     ## Quick Run
     # Experiment(base_dir).run_simulation(num_nodes=200, num_connections=2000, output_dir="graphtools", 
-    #                                     num_steps=200000, display_interval=50000, metrics_interval=1000, random_seed=42, color_by=ColorBy.CLUSTER)
+    #                                     num_steps=20000, display_interval=5000, metrics_interval=1000, random_seed=42, color_by=ColorBy.ACTIVITY)
 
     # Experiment Run
     experiment_folder = os.path.join(base_dir, "new_graphtools_stuff")
@@ -34,13 +34,13 @@ if __name__ == "__main__":
     experiment.run_experiment(
                             seed_range=range(5),
                             nodes_range=[200],
-                            connections_range=range(1500, 8000, 250),      # [x / 100.0 for x in range(2, 40, 2)],
+                            connections_range=range(250, 19_901, 250),      # [x / 100.0 for x in range(2, 40, 2)],
                             connections_as_density = False,
-                            num_steps=3_0_000,
+                            num_steps=3_000_000,
                             display_interval=100_000,
                             metrics_interval=1_000,
                         )
-    PostRunAnalyzer(experiment_folder).aggregate_metrics(experiment_folder, starting_step=2_000_000)
+    # PostRunAnalyzer(experiment_folder).aggregate_metrics(experiment_folder, starting_step=2_000_000)
 
     if profiler: profiler.disable()
 
