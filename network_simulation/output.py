78af6b10ab031d3b20cd6532e1febe9a03a859e7
BaronBrixius <kiqjaq@gmail.com>
2025-01-05 16:38:30 +0100

    Cleaning

--- network_simulation/output.py
+++ network_simulation/output.py
@@ -1,32 +1,24 @@
 import csv
-import logging
 import os
-from network_simulation.metrics import Metrics
 from network_simulation.network import NodeNetwork
-import networkx as nx
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
 class Output:
     def __init__(self, project_dir, num_nodes=None, num_connections=None):
-        self.logger = logging.getLogger(__name__)
         self.num_nodes = num_nodes
         self.num_connections = num_connections
 
         self.project_dir = project_dir
         os.makedirs(project_dir, exist_ok=True)
 
-        self.metrics_file_path = os.path.join(self.project_dir, f"summary_metrics_nodes_{self.num_nodes}_edges_{self.num_connections}.csv")
-        print(self.metrics_file_path)
-        self.metrics_file = open(self.metrics_file_path, mode="w", newline="")
+        metrics_file_path = os.path.join(self.project_dir, f"summary_metrics_nodes_{self.num_nodes}_edges_{self.num_connections}.csv")
+        self.metrics_file = open(metrics_file_path, mode="w", newline="")
         self.csv_writer = None
 
     # Runtime Metrics Writing
     def write_metrics_line(self, step, network: NodeNetwork):
         row = self._compute_row(step, network)
 
-        if self.csv_writer is None:
+        if self.csv_writer is None: # Lazy loading the writer also allows us to wait and see what headers will be needed
             self.csv_writer = csv.DictWriter(self.metrics_file, fieldnames=row.keys())
             self.csv_writer.writeheader()
 
@@ -38,9 +30,6 @@ class Output:
         metrics = network.metrics
         activities = network.activities.a
 
-        # Get cluster metrics
-        cluster_metrics = metrics.get_cluster_metrics(graph, step)
-
         # Compute row data
         row = {
             "Step": step,
@@ -50,7 +39,7 @@ class Output:
         }
 
         # Update with cluster metrics
-        row.update(cluster_metrics)
+        row.update(metrics.get_cluster_metrics(graph, step))
 
         # Add rewiring metrics
         row.update(
