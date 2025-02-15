3f720c61eb36fd653b7c0cbc7a91dc331ae919cc
BaronBrixius <kiqjaq@gmail.com>
2025-02-15 11:51:36 +0100

    Filepath handling in Experiment

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ if __name__ == "__main__":
     # NetworkControlApp()
 
     ## Quick Run
-    Experiment(base_dir).run_one_simulation(num_nodes=200, num_connections=4000, output_dir="gpu", 
-                                        num_steps=30_000, display_interval=0, metrics_interval=1000, random_seed=0, color_by=ColorBy.DEGREE)
+    Experiment().run_one_simulation(num_nodes=200, num_connections=4000, simulation_dir=os.path.join(base_dir, "gpu"), 
+                                        num_steps=30_000, display_interval=1000, metrics_interval=1000, random_seed=0, color_by=ColorBy.DEGREE)
 
     # Experiment Run
-    # experiment_folder = os.path.join(base_dir, "testexp")
-    # Experiment(experiment_folder).run_experiment(
+    # experiment_folder = os.path.join(base_dir, "hybrid_rainbow_zero")
+    # Experiment().run_experiment(
     #                         seed_range=range(1),
     #                         nodes_range=[200],
     #                         connections_range=range(2000, 4001, 1000),      # [x / 100.0 for x in range(2, 40, 2)],
@@ -37,6 +37,7 @@ if __name__ == "__main__":
     #                         display_interval=0,
     #                         metrics_interval=1_000,
     #                         color_by=ColorBy.CLUSTER,
+    #                         experiment_folder=experiment_folder
     #                     )
     # PostRunAnalyzer(experiment_folder).aggregate_metrics(experiment_folder)
 
--- network_simulation/experiment.py
+++ network_simulation/experiment.py
@@ -7,8 +7,7 @@ import threading
 import multiprocessing
 
 class Experiment:
-    def __init__(self, experiment_folder):
-        self.experiment_folder = experiment_folder
+    def __init__(self):
         self.termination_flag = multiprocessing.Manager().Event()
         self.logger = logging.getLogger(__name__)
 
@@ -21,17 +20,17 @@ class Experiment:
                 self.logger.info("Termination signal received. Slow-stopping experiment...")
                 break
 
-    def run_one_simulation(self, num_nodes, num_connections, output_dir, num_steps, display_interval, metrics_interval, random_seed, color_by=ColorBy.ACTIVITY):
+    def run_one_simulation(self, num_nodes, num_connections, simulation_dir, num_steps, display_interval, metrics_interval, random_seed, color_by=ColorBy.ACTIVITY):
         if self.termination_flag.is_set():
             return f"Simulation {random_seed, num_nodes, num_connections} terminated by user"
 
         from network_simulation.simulation import Simulation  # Import inside to ensure clean process
         self.logger.info(f"Starting with {random_seed, num_nodes, num_connections}")
-        sim = Simulation(num_nodes=num_nodes, num_connections=num_connections, output_dir=output_dir, color_by=color_by, random_seed=random_seed)
+        sim = Simulation(num_nodes=num_nodes, num_connections=num_connections, output_dir=simulation_dir, color_by=color_by, random_seed=random_seed)
         sim.run(num_steps=num_steps, display_interval=display_interval, metrics_interval=metrics_interval)
         return f"Simulation completed for {random_seed, num_nodes, num_connections}"
 
-    def run_experiment(self, seed_range, nodes_range, connections_range, num_steps, display_interval, metrics_interval, color_by=ColorBy.ACTIVITY):
+    def run_experiment(self, seed_range, nodes_range, connections_range, num_steps, display_interval, metrics_interval, color_by=ColorBy.ACTIVITY, experiment_dir="/mnt/d/OneDrive - Vrije Universiteit Amsterdam/Y3-Thesis/code/output"):
         # Start thread that checks for early termination
         threading.Thread(target=self.monitor_input_early_termination, daemon=True).start()
 
@@ -44,7 +43,7 @@ class Experiment:
                     num_connections = int(num_connections * (num_nodes * (num_nodes - 1) / 2))
 
                 # Skip if scenario is already completed
-                simulation_dir = os.path.join(self.experiment_folder, f"seed_{seed}", f"nodes_{num_nodes}", f"edges_{num_connections}")
+                simulation_dir = os.path.join(experiment_dir, f"seed_{seed}", f"nodes_{num_nodes}", f"edges_{num_connections}")
                 if os.path.exists(simulation_dir):
                     self.logger.info(f"Skipping completed scenario: {seed, num_nodes, num_connections}")
                     continue
