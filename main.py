60e263b9fa5aa2a1e9434c31c0abe69b75ead70a
BaronBrixius <kiqjaq@gmail.com>
2025-02-18 01:50:02 +0100

    little_buster start?

--- main.py
+++ main.py
@@ -17,34 +17,34 @@ if __name__ == "__main__":
             datefmt="%H:%M:%S"
         )
 
-    profiler = cProfile.Profile()
+    profiler = None #cProfile.Profile()
     if profiler: profiler.enable()
 
     ## Run in GUI
     # NetworkControlApp()
 
     ## Quick Run
-    Experiment().run_one_simulation(num_nodes=200, num_connections=2000, simulation_dir=os.path.join(base_dir, "maybegpu"), 
-                                        num_steps=10_000, display_interval=1_000, metrics_interval=1000, random_seed=0, color_by=ColorBy.DEGREE)
+    # Experiment().run_one_simulation(num_nodes=200, num_connections=2000, simulation_dir=os.path.join(base_dir, "heywegpu"), 
+    #                                     num_steps=40_000, display_interval=10_000, metrics_interval=1000, random_seed=0, color_by=ColorBy.DEGREE)
 
     # Experiment Run
-    # experiment_folder = os.path.join(base_dir, "gpu_parallel")
-    # Experiment().run_experiment(
-    #                         seed_range=range(1),
-    #                         nodes_range=[200],
-    #                         connections_range=range(2000, 10001, 1000),      # [x / 100.0 for x in range(2, 40, 2)],
-    #                         num_steps=10_000,
-    #                         display_interval=1000,
-    #                         metrics_interval=1_000,
-    #                         color_by=ColorBy.DEGREE,
-    #                         experiment_dir=experiment_folder
-    #                     )
+    experiment_folder = os.path.join(base_dir, "little_buster")
+    Experiment().run_experiment(
+                            seed_range=range(5, 8),
+                            nodes_range=[200],
+                            connections_range=range(2000, 10001, 100),      # [x / 100.0 for x in range(2, 40, 2)],
+                            num_steps=10_000_000,
+                            display_interval=100_000,
+                            metrics_interval=1_000,
+                            color_by=ColorBy.DEGREE,
+                            experiment_dir=experiment_folder
+                        )
     # PostRunAnalyzer(experiment_folder).aggregate_metrics(experiment_folder)
 
     if profiler: profiler.disable()
 
     # Print profiler stats to sort by cumulative time
-    if profiler: pstats.Stats(profiler).strip_dirs().sort_stats("cumulative").print_stats(10)
+    if profiler: pstats.Stats(profiler).strip_dirs().sort_stats("cumulative").print_stats(20)
 
     print(get_times())   #TODO Remove for final version
     # print(Metrics.get_cluster_assignments.cache_info())
