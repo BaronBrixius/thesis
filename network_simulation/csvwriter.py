4cb412557b4abf8d51c5da3628418d225e5443b5
BaronBrixius <kiqjaq@gmail.com>
2025-02-27 17:49:08 +0100

    simple and clean

--- network_simulation/csvwriter.py
+++ network_simulation/csvwriter.py
@@ -8,12 +8,10 @@ class CSVWriter:
         self.metrics_file = open(metrics_file_path, mode="w", newline="")
         self.csv_writer = None  # Initialize lazily on first write
 
-    # Runtime Metrics Writing
     def write_metrics_line(self, row):
         if self.csv_writer is None:  # Set fieldnames on first write
             self.csv_writer = csv.DictWriter(self.metrics_file, fieldnames=row.keys())
-            if self.metrics_file.tell() == 0:  # Only write headers if file is empty
-                self.csv_writer.writeheader()
+            self.csv_writer.writeheader()
         self.csv_writer.writerow(row)
 
     def close(self):
