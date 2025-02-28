ca25f8689bd1957d7bf771f03aab01ce29773b15
BaronBrixius <kiqjaq@gmail.com>
2025-02-28 03:20:56 +0100

    Moved some setup from main loop to init

--- network_simulation/experiment.py
+++ network_simulation/experiment.py
@@ -21,8 +21,8 @@ class Experiment:
     def run_one_simulation(self, num_nodes, num_edges, simulation_dir, num_steps, display_interval, metrics_interval, random_seed, color_by, process_num=0):
         from network_simulation.simulation import Simulation  # Import inside to ensure clean process
         print(f"Starting simulation {random_seed, num_nodes, num_edges}") # I don't want to reimport the logger just for this, remove this for final?
-        sim = Simulation(num_nodes=num_nodes, num_edges=num_edges, simulation_dir=simulation_dir, color_by=color_by, random_seed=random_seed, process_num=process_num)
-        sim.run(num_steps=num_steps, display_interval=display_interval, metrics_interval=metrics_interval)
+        sim = Simulation(num_nodes=num_nodes, num_edges=num_edges, simulation_dir=simulation_dir, color_by=color_by, display_interval=display_interval, metrics_interval=metrics_interval, random_seed=random_seed, process_num=process_num)
+        sim.run(num_steps=num_steps)
         return f"Completed simulation {random_seed, num_nodes, num_edges}"
 
     def run_experiment(self, seed_range, nodes_range, edges_range, num_steps, display_interval, metrics_interval, color_by, experiment_dir="/mnt/d/OneDrive - Vrije Universiteit Amsterdam/Y3-Thesis/code/output"):
--- network_simulation/simulation.py
+++ network_simulation/simulation.py
@@ -5,38 +5,45 @@ from network_simulation.blockmodel import BlockModel
 import network_simulation.metrics as Metrics
 
 class Simulation:
-    def __init__(self, num_nodes, num_edges, color_by=ColorBy.ACTIVITY, simulation_dir=None, alpha=1.7, epsilon=0.4, random_seed=None, process_num=0):
+    def __init__(self, num_nodes, num_edges, color_by=ColorBy.ACTIVITY, simulation_dir=None, alpha=1.7, epsilon=0.4, display_interval=1000, metrics_interval=1000, random_seed=None, process_num=0):
+        # Simulation Parameters
+        self.display_interval = display_interval
+        self.metrics_interval = metrics_interval
+
+        # Initialize Network
         self.network = NodeNetwork(num_nodes, num_edges, alpha, epsilon, random_seed, process_num)
         adjacency_matrix, activities = self.network.get_adjacency_matrix(), self.network.get_activities()
+
+        # Initialize Simulation Components
         self.output = CSVWriter(simulation_dir)
         self.block_model = BlockModel(adjacency_matrix)
         self.visualization = Visualization(adjacency_matrix, activities, self.block_model.get_graph(), self.block_model.get_community_assignments(), simulation_dir, color_by)
 
-    def run(self, num_steps, display_interval=1000, metrics_interval=1000):
-        adjacency_matrix, activities = self.network.get_adjacency_matrix(), self.network.get_activities()
+        # Initial Output
+        self._handle_output(adjacency_matrix, activities, self.block_model, 0)
 
-        # Main Loop
+    def run(self, num_steps):
+        """Main loop for the simulation."""
         step = 0
         while step < num_steps:
-            # Output Metrics and Visualization
-            self._handle_output(adjacency_matrix, activities, self.block_model, step, display_interval, metrics_interval)
-
             # Update Network
-            iterations_to_next_interval = min(num_steps - step, display_interval - step % display_interval if display_interval else float('inf'), metrics_interval - step % metrics_interval if metrics_interval else float('inf'))
+            iterations_to_next_interval = min(num_steps - step, self.display_interval - step % self.display_interval if self.display_interval else float('inf'), self.metrics_interval - step % self.metrics_interval if self.metrics_interval else float('inf'))
             adjacency_matrix, activities = self.network.update_network(iterations_to_next_interval)
             step += iterations_to_next_interval
 
             # Update Block Model
             self.block_model.update_block_model(adjacency_matrix, step)
 
+            # Output Metrics and Visualization
+            self._handle_output(adjacency_matrix, activities, self.block_model, step)
+
         # Final Output
-        self._handle_output(adjacency_matrix, activities, self.block_model, step, display_interval, metrics_interval)
         self.output.close()
 
-    def _handle_output(self, adjacency_matrix, activities, block_model, step, display_interval, metrics_interval):
+    def _handle_output(self, adjacency_matrix, activities, block_model, step):
         """Checks and handles display and metrics intervals."""
-        if metrics_interval and step % metrics_interval == 0:
+        if self.metrics_interval and step % self.metrics_interval == 0:
             self.output.write_metrics_line(Metrics.compute_all_metrics(adjacency_matrix, block_model.get_graph(), block_model.get_entropy(), block_model.get_community_assignments(), step))
 
-        if display_interval and step % display_interval == 0:
-            self.visualization.draw_visual(adjacency_matrix, activities, block_model.get_graph(), block_model.get_community_assignments(), step, max_iter=display_interval)
+        if self.display_interval and step % self.display_interval == 0:
+            self.visualization.draw_visual(adjacency_matrix, activities, block_model.get_graph(), block_model.get_community_assignments(), step, max_iter=self.display_interval)
